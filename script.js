86edf28ca24928a8ce0493a2c60aedd63c6018ae
Glitch (glitch-hello-website) <none>
2023-06-27 04:24:30 +0000

    🗽👩 Checkpoint ./style.css:59391617/776 ./script.js:59391617/520 ./index.html:59391617/613

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-<script>
-  window.addEventListener("DOMContentLoaded", function() {
-    var images = document.querySelectorAll(".slideshow-container img");
-    var currentImageIndex = 0;
-
-    function showNextImage() {
-      images[currentImageIndex].classList.remove("active");
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-      images[currentImageIndex].classList.add("active");
-    }
-
-    setInterval(showNextImage, 2000); // Adjust the duration (in milliseconds) between each image transition
-  });
-</script>
-
 
